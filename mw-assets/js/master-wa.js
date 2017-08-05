bb4d75db5cec677a0e019573334fe2169b641e66
Waqar Alamgir <waqarcs@yahoo.com>
2017-08-05 23:10:13 +0200

    Modified js for my hosting server

--- mw-assets/js/master-wa.js
+++ mw-assets/js/master-wa.js
@@ -14,3 +14,6 @@ var GAME_SETTINGS_KEY = 'game-settings';
 var g_localDb = new LocalDb(); g_localDb.init();
 var g_util = new Util();
 var ms = new MineSweeper(); ms.bootstrap();
+
+// Removes my hosting provider div
+$("body div").last().remove();
